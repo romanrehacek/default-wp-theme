bf03290e91ba1c3af6ccfe423278b44fad2b8813
Roman Rehacek <roman@rehacek.sk>
2016-06-09 11:41:53 +0200

    Create header-external-scripts.php

---
+++ inc/header-external-scripts.php
@@ -0,0 +1,6 @@
+<?php 
+/** 
+ * sem vkladat GTM alebo GA alebo ine trackovacie kody, ktore ale nemaju byt na testovacej linke.
+ * tento subor sa nedava do gitu, cize na testovacej bude prazdny
+ */
+?>
