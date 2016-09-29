4f0300968e6b7e4a68a280f34cba4f11022d37cb
Roman Rehacek <roman@rehacek.sk>
2016-09-29 09:40:38 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -27,15 +27,21 @@ function r_theme_defaults__remove_menus(){
     remove_menu_page( 'tools.php' );    //Tools
 }
 
+$dev = true;	// set false in production
+if ($dev) {
+	$min = '';
+} else {
+	$min = '.min';
+}
 
 function r_theme_defaults__load_styles() {
-	wp_enqueue_style('',                get_template_directory_uri() . '/css/',       false, '');
-    wp_enqueue_style('main.min',		get_template_directory_uri() . '/css/main.min.css',       false, filemtime( get_stylesheet_directory() . '/css/main.min.css' ));
+	wp_enqueue_style('plugins.min',     get_template_directory_uri() . '/css/plugins'.$min.'.css',    false, filemtime( get_stylesheet_directory() . '/css/plugins'.$min.'.css' ));
+    wp_enqueue_style('main.min',		get_template_directory_uri() . '/css/main'.$min.'.css',       false, filemtime( get_stylesheet_directory() . '/css/main'.$min.'.css' ));
 }
 
 function r_theme_defaults__load_scripts() {
-	wp_enqueue_script('',				get_template_directory_uri(). '/js/',	array('jquery'), '', true);
-    wp_enqueue_script('main.min',		get_template_directory_uri(). '/js/main.min.js',	array('jquery'), filemtime( get_stylesheet_directory() . '/js/main.min.js' ), true);
+	wp_enqueue_script('plugins.min',	get_template_directory_uri(). '/js/plugins'.$min.'.js',	array('jquery'), filemtime( get_stylesheet_directory() . '/js/plugins'.$min.'.js' ), true);
+    wp_enqueue_script('main.min',		get_template_directory_uri(). '/js/main'.$min.'.js',	array('jquery'), filemtime( get_stylesheet_directory() . '/js/main'.$min.'.js' ), true);
     
     wp_localize_script( 'main.min', 'global_data', array( 
     							'ajax_url' => admin_url( 'admin-ajax.php' ), 
