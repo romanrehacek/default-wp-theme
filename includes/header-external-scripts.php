d680980bf87605608623a4abe0bf87b663a11f6a
Roman Rehacek <roman@rehacek.sk>
2017-05-25 10:11:48 +0000

    Remove deprecated file

--- includes/header-external-scripts.php
+++
@@ -1,6 +0,0 @@
-<?php 
-/** 
- * sem vkladat GTM alebo GA alebo ine trackovacie kody, ktore ale nemaju byt na testovacej linke.
- * tento subor sa nedava do gitu, cize na testovacej bude prazdny
- */
-?>
