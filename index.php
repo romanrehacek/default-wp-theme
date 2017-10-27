152d8d42f9b2f158f0e2b57c13f511fabb9247b7
Roman Rehacek <roman@rehacek.sk>
2017-10-27 11:10:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-<?php
-get_header();
+<?php get_header(); ?>
 
-if (have_posts()) {
-	while (have_posts()) {
-		the_post();
-		the_content();
-	}
-}
+<?php if (have_posts()) : ?>
+	<?php while (have_posts()) : the_post(); ?>
+		<?php the_content(); ?>
+	<?php endwhile; ?>
+<?php endif; ?>
 
-get_footer();
+<?php get_footer(); ?>
